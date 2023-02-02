c1f967003731da5c883b378a8ecca345acfaf4bf
aBock4 <arbockhorst@gmail.com>
2023-02-02 16:19:25 -0500

    Added two different ways of pushing model to view

--- plantplaces/src/main/java/com/plantplaces/PlantPlacesController.java
+++ plantplaces/src/main/java/com/plantplaces/PlantPlacesController.java
@@ -1,19 +1,30 @@
 package com.plantplaces;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.plantplaces.dto.SpecimenDTO;
+import com.plantplaces.service.ISpecimenService;
 
 @Controller
 public class PlantPlacesController {
 
+	@Autowired
+	private ISpecimenService specimenServiceStub;
+	
 	/**
 	 * Handle the /start endpoint
 	 * @return
 	 */
 	@RequestMapping(value="/start", method=RequestMethod.GET)
-	public String read() {
+	public String read(Model model) {
+		SpecimenDTO specimenDTO = specimenServiceStub.fetchById(43);
+		model.addAttribute("specimenDTO",specimenDTO);
 		return "start";
 	}
 	
@@ -28,8 +39,13 @@ public class PlantPlacesController {
 	}
 	
 	@RequestMapping(value="/start", method=RequestMethod.GET, params= {"loyalty=silver"})
-	public String readSilver() {
-		return "start";
+	public ModelAndView readSilver() {
+		SpecimenDTO specimenDTO = specimenServiceStub.fetchById(43);
+		specimenDTO.setSpecimenId(83);
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("start");
+		modelAndView.addObject("specimenDTO", specimenDTO);
+		return modelAndView;
 	}
 	
 	@PostMapping("/start")
