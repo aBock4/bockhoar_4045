660a97622ad58d566668b8078390e6a523b2f142
aBock4 <arbockhorst@gmail.com>
2023-02-02 17:08:34 -0500

    Added @RequestParam to endpoint

--- plantplaces/src/main/java/com/plantplaces/PlantPlacesController.java
+++ plantplaces/src/main/java/com/plantplaces/PlantPlacesController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -32,6 +33,14 @@ public class PlantPlacesController {
 	
 	@RequestMapping(value="/start", method=RequestMethod.GET, headers={"content-type=text/json"})
 	public String readJSON() {
+		return "start";
+	}
+	
+	@RequestMapping(value="/addspecimen", method=RequestMethod.GET)
+	public String addSpecimen(Model model, @RequestParam(value="latitude", required=false, defaultValue="0.0")String latitude) {
+		SpecimenDTO specimenDTO = specimenServiceStub.fetchById(43);
+		specimenDTO.setLatitude(latitude);
+		model.addAttribute("specimenDTO",specimenDTO);
 		return "start";
 	}
 	
